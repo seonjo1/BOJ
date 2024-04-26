f88dcf28acb6b14299e14b468ffd4bac1edf46b2
seonjo1 <tjdgus9684@gmail.com>
2024-04-26 17:52:09 +0900

    Update BOJ3080 : 일단 구현 성공

--- BOJ3080/main.cpp
+++ BOJ3080/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int sum;
+long long factorial[27];
+long long sum = 1;
 
 struct Tri
 {
 	bool finish;
 	int child;
+	char *str;
 	Tri* next[26];
 
-	Tri() : finish(false), child(0)
+	Tri() : finish(false), child(0), str(NULL)
 	{
 		memset(next, 0, sizeof(next));
+		if (str) delete str;
 	}
 
 	~Tri()
@@ -23,22 +28,50 @@ struct Tri
 			delete next[i];
 	}
 
-	void insert(const char *key)
+	void insert(const char *key, bool go)
 	{
 		if (*key == '\0')
 			finish = true;
-		else
+		else if (go)
 		{
-			int i = *key - 'a';
+			if (str != NULL)
+			{
+				char *tmp = str;
+				str = NULL;
+				finish = false;
+				int i = *tmp - 'A';
+				this->insert(tmp, true);
+				delete tmp;
+			}
+			int i = *key - 'A';
 			if (!next[i])
 			{
 				child++;
 				next[i] = new Tri();
+				next[i]->insert(key + 1, false);
 			}
-			next[i]->insert(key + 1);
+			else next[i]->insert(key + 1, true);
+		}
+		else
+		{
+			finish = true;
+			str = strdup(key);
 		}
 	}
 
+	void getAnswer()
+	{
+		if (child)
+		{
+			if (finish) sum = ((factorial[child + 1] * sum) % 1000000007);
+			else sum = ((factorial[child] * sum) % 1000000007);
+		}
+		for (int i = 0; i < 26; i++)
+		{
+			if (next[i] != NULL)
+				next[i]->getAnswer();
+		}
+	}
 
 };
 
@@ -48,4 +81,25 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
+	factorial[0] = 1;
+	for (int i = 1; i < 27; i++)
+		factorial[i] = (factorial[i - 1] * i) % 1000000007;
+	
+	int N;
+	cin >> N;
+	vector<string> v;
+
+	string str;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+		v.push_back(str);
+	}
+	sort(v.begin(), v.end());
+
+	Tri tri;
+	for (int i = 0; i < N; i++)
+		tri.insert(v[i].c_str(), true);
+	tri.getAnswer();
+	cout << sum << endl;
 }
